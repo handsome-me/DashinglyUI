216febcf58f44badd0e75ce1a26b78e07fc580bc
Meghraj <meghraj.deshmukh103@gmail.com>
2022-02-24 15:16:51 +0530

    some clean

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -3,15 +3,26 @@ import PropTypes from 'prop-types';
 import { css, cx,CSSStyleSheet } from '@emotion/css'
 
 
+const styledButton=()=>{
+
+    return css({
+       display:'inline-flex',
+       padding:"20px",
+       backgroundColor:"green"
+    })
+
+}
+
+
 const Default_Style = css({
     color:"red",
-    
+   
   })
 const Button = (props:any) => {
     return (
-        <div className={Default_Style}>
+        <button className={styledButton()}>
             Meghraj_Deshmukh
-        </div>
+        </button>
     );
 };
 
