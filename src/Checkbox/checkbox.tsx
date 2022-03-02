d05e05225578561c55b4dc741056d5ef97c27dfc
Meghraj <meghraj.deshmukh103@gmail.com>
2022-03-02 23:35:34 +0530

    structure of form component

--- src/Checkbox/checkbox.tsx
+++ src/Checkbox/checkbox.tsx
@@ -25,6 +25,18 @@ const styleInput=()=>{
        }
     })
 }
+<FormControl>
+      <FormLabel id="demo-radio-buttons-group-label">Gender</FormLabel>
+      <RadioGroup
+        aria-labelledby="demo-radio-buttons-group-label"
+        defaultValue="female"
+        name="radio-buttons-group"
+      >
+        <FormControlLabel value="female" control={<Radio />} label="Female" />
+        <FormControlLabel value="male" control={<Radio />} label="Male" />
+        <FormControlLabel value="other" control={<Radio />} label="Other" />
+      </RadioGroup>
+    </FormControl>
 const styleDiv=()=>{
    return css({
      "::before":{
