b17086f21004e206b28c13c13839ff7a4bfc813b
Meghraj <meghraj.deshmukh103@gmail.com>
2022-03-05 13:38:42 +0530

    radiobox created

---
+++ src/RadioBox/radio.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { css } from '@emotion/css';
+
+interface Props{
+
+}
+
+const wrapper=()=>{
+    
+}
+
+const styleRadio=()=>{
+   
+    return css({
+    
+
+        
+    })
+    
+}
+
+const radio = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+radio.propTypes = {
+    
+};
+
+export default radio;
