9dbd2ac52aef653ba75c4271cfa134c61e0b6e77
Meghraj <meghraj.deshmukh103@gmail.com>
2022-03-05 13:57:54 +0530

    svg material ui

--- src/RadioBox/radio.tsx
+++ src/RadioBox/radio.tsx
@@ -1,30 +1,70 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { css } from '@emotion/css';
-
+import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonUnchecked';
 interface Props{
 
 }
+let   STYLE_TYPE:"WrapperStyle" | "svgWrapperStyle" | "inputStyle";
 
-const wrapper=()=>{
-    
-}
-
-const styleRadio=()=>{
-   
+ 
+const wrapperStyle=function():string{
     return css({
-    
-
+        position:"relative",
+        display:'inline-flex',
+        border:"1px solid ",
+        borderRadius:'50%',
+        color:'pink'
+    })
+}
+const svgWrapperStyle=function():string{
+    return css({
+        position:'relative',
+        display:'flex',
+        color:'pink'
+    })
+}
+const inputStyle=function():string{
+    return css({
+        position:'absolute',
+        opacity:0,
+        zIndex:1,
+        top:0,
+        bottom:0,
+        width:'100%',
+        height:'100%',
         
     })
+}
+const styleRadio=(type:typeof STYLE_TYPE)=>{
+     
+     
+    switch(type){
+
+        case "WrapperStyle":{
+            return wrapperStyle();
+        }
+        case "inputStyle":{
+            return inputStyle();
+        }
+        case "svgWrapperStyle":{
+            return svgWrapperStyle();
+        }
+      
+        
+    }
     
 }
 
 const radio = () => {
     return (
-        <div>
-            
-        </div>
+        <span className={styleRadio("WrapperStyle")}>
+         <input className={styleRadio('inputStyle')}>
+         </input>
+         <span className={styleRadio("svgWrapperStyle")}>
+             <RadioButtonUncheckedIcon ></RadioButtonUncheckedIcon>
+         </span>
+        </span>
     );
 };
 
